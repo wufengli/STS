faacc161da99ae77050298298a6cd1a5148d2d7e
tianluoluo <tianluoluo666@gmail.com>
2018-12-20 10:38:28 +0800

    'luo'

--- sts-pc/sts/src/store/action.js
+++
@@ -1,6 +0,0 @@
-const actions = {
-  setId: (context) => {
-    context.commit('setUserId')
-  }
-}
-export default actions
--- sts-pc/sts/src/store/getters.js
+++
@@ -1,9 +0,0 @@
-const getters = {
-  getUserId: state => {
-    return state.userId
-  },
-  getUserName: state => {
-    return state.userName
-  }
-}
-export default getters
