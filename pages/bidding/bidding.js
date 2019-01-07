fbe6209de8b920c7a29073bc3b5c0a8976c60532
tianluoluo <tianluoluo666@gmail.com>
2019-01-07 15:59:03 +0800

    'luo'

--- pages/bidding/bidding.js
+++ pages/bidding/bidding.js
@@ -1,69 +1,24 @@
 Page({
-
   data: {
+    biddingpageSize: 2,
+    biddingtotal: '',
+    biddedpageSize: 2,
+    biddedtotal: '',
+    total: '',
+    pick: '',
+    deliver: '',
+    // userId为默认值wbcys001
+    userId: 'u0006',
     hiddenmodalput: true,
-    bidTaskNum: '',
+    bidTaskId: '',
     quotedPrice: '',
     placeholder: '请输入您的竞拍价',
     tabs: ["未竞价", "已竞价"],
     activeIndex: 0,
-    biddinglist: [{
-      bidTaskNum: "RW181221162007016",
-      releaseTime: "2018-12-21 16:20:13",
-      pickArea: "湖北省-天门市-天门市",
-      deliverArea: "湖北省-天门市-天门市",
-      taskType: "冷藏",
-      serviceTime: "78",
-      totalQuantity: "2",
-      totalWeight: "33",
-      totalVolume: "22",
-      proposedPrice: "133",
-      sealedDiskTime: "50"
-    }, {
-      bidTaskNum: "RW181221162007015",
-      releaseTime: "2018-12-21 16:20:13",
-      pickArea: "湖北省-天门市-天门市",
-      deliverArea: "湖北省-天门市-天门市",
-      taskType: "冷藏",
-      serviceTime: "78",
-      totalQuantity: "2",
-      totalWeight: "33",
-      totalVolume: "22",
-      proposedPrice: "133",
-      sealedDiskTime: "4000"
-    }],
-    biddedlist: [{
-      bidTaskNum: "RW181221162007017",
-      releaseTime: "2018-12-21 16:20:13",
-      pickArea: "湖北省-天门市-天门市",
-      deliverArea: "湖北省-天门市-天门市",
-      taskType: "冷藏",
-      serviceTime: "78",
-      totalQuantity: "2",
-      totalWeight: "33",
-      totalVolume: "22",
-      proposedPrice: "133",
-      quotedPrice: "124",
-      sealedDiskTime: "50"
-    }, {
-      bidTaskNum: "RW181221162007018",
-      releaseTime: "2018-12-21 16:20:13",
-      pickArea: "湖北省-天门市-天门市",
-      deliverArea: "湖北省-天门市-天门市",
-      taskType: "冷藏",
-      serviceTime: "78",
-      totalQuantity: "2",
-      totalWeight: "33",
-      totalVolume: "22",
-      proposedPrice: "133",
-      quotedPrice: "124",
-      sealedDiskTime: "110"
-    }]
+    biddedlist: [],
+    biddinglist: []
   },
   onLoad: function(options) {
-    wx.setNavigationBarTitle({
-      title: '竞价操作'
-    })
     var that = this;
     wx.getSystemInfo({
       success: function(res) {
@@ -72,90 +27,97 @@ Page({
         });
       }
     })
+    // 获取未竞价数据
+    this.ajaxBidding();
+    // 获取已竞价数据
+    this.ajaxBidded();
     // 执行倒计时函数
     this.countDown();
   },
+  // 格式化倒计时数据格式
   timeFormat(param) {
     return param < 10 ? '0' + param : param;
   },
+  // 倒计时函数
   countDown() {
-    this.data.biddinglist.forEach((o, index) => {
-      if (typeof o.sealedDiskTime === 'object') {
-        o.sealedDiskTime = parseInt(o.sealedDiskTime.sec) + parseInt(o.sealedDiskTime.min) * 60 + parseInt(o.sealedDiskTime.hou * 60 * 60);
-        o.sealedDiskTime = o.sealedDiskTime - 1;
-      } else {
-        o.sealedDiskTime = parseInt(o.sealedDiskTime);
-      }
-      let time = o.sealedDiskTime;
-      let obj = null;
-      if (time >= 0) {
-        let hou = Math.floor(time / 60 / 60 % 24);
-        let min = Math.floor(time / 60 % 60);
-        let sec = Math.floor(time % 60);
-        obj = {
-          hou: this.timeFormat(hou),
-          min: this.timeFormat(min),
-          sec: this.timeFormat(sec)
-        }
-      } else {
-        obj = {
-          hou: '00',
-          min: '00',
-          sec: '00'
-        }
-        this.data.biddinglist.splice(index, 1);
-      }
-      o.sealedDiskTime = obj;
-    })
+    this.timeCount(this.data.biddinglist)
     // 渲染，然后每隔一秒执行一次倒计时函数
     this.setData({
       biddinglist: this.data.biddinglist
     })
-    this.data.biddedlist.forEach((o, index) => {
-      if (typeof o.sealedDiskTime === 'object') {
-        o.sealedDiskTime = parseInt(o.sealedDiskTime.sec) + parseInt(o.sealedDiskTime.min) * 60 + parseInt(o.sealedDiskTime.hou * 60 * 60);
-        o.sealedDiskTime = o.sealedDiskTime - 1;
-      } else {
-        o.sealedDiskTime = parseInt(o.sealedDiskTime);
-      }
-      let time = o.sealedDiskTime;
-      let obj = null;
-      if (time >= 0) {
-        let hou = Math.floor(time / 60 / 60 % 24);
-        let min = Math.floor(time / 60 % 60);
-        let sec = Math.floor(time % 60);
-        obj = {
-          hou: this.timeFormat(hou),
-          min: this.timeFormat(min),
-          sec: this.timeFormat(sec)
-        }
-      } else {
-        obj = {
-          hou: '00',
-          min: '00',
-          sec: '00'
-        }
-        this.data.biddedlist.splice(index, 1);
-      }
-      o.sealedDiskTime = obj;
-    })
+    this.timeCount(this.data.biddedlist)
     // 渲染，然后每隔一秒执行一次倒计时函数
     this.setData({
       biddedlist: this.data.biddedlist
     })
     setTimeout(this.countDown, 1000);
   },
+  // 计时列表渲染
+  timeCount(e) {
+    e.forEach((o, index) => {
+      if (typeof o.sealedDiskTime === 'object') {
+        o.sealedDiskTime = parseInt(o.sealedDiskTime.sec) + parseInt(o.sealedDiskTime.min) * 60 + parseInt(o.sealedDiskTime.hou * 60 * 60);
+        o.sealedDiskTime = o.sealedDiskTime - 1;
+      } else {
+        o.sealedDiskTime = parseInt(o.sealedDiskTime);
+      }
+      let time = o.sealedDiskTime;
+      let obj = null;
+      if (time >= 0) {
+        let hou = Math.floor(time / 60 / 60 % 24);
+        let min = Math.floor(time / 60 % 60);
+        let sec = Math.floor(time % 60);
+        obj = {
+          hou: this.timeFormat(hou),
+          min: this.timeFormat(min),
+          sec: this.timeFormat(sec)
+        }
+      } else {
+        obj = {
+          hou: '00',
+          min: '00',
+          sec: '00'
+        }
+        e.splice(index, 1);
+      }
+      o.sealedDiskTime = obj;
+    })
+  },
+  // search
+  searchInput(e) { 
+    this.setData({
+      pick: e.detail.value
+    })
+    if (parseInt(this.data.activeIndex) === 0) {
+      this.setData({
+        biddingpageSize: 2
+      })
+      this.ajaxBidding();
+    } else {
+      this.setData({
+        biddedpageSize: 2
+      })
+      this.ajaxBidded();
+    }
+  },
+  clearInput(e) { 
+    this.setData({
+      pick: ''
+    })
+  },
+  // 切换tab
   tabClick: function(e) {
     this.setData({
       sliderOffset: e.currentTarget.offsetLeft,
       activeIndex: e.currentTarget.id
     });
   },
+  // 竞价
   bidding: function(e) {
     this.setData({
       quotedPrice: '',
       hiddenmodalput: !this.data.hiddenmodalput,
-      bidTaskNum: e.currentTarget.id
+      bidTaskId: e.currentTarget.id
     })
   },
   //获取输入价格
@@ -172,21 +134,119 @@ Page({
   },
   //确认  
   confirm: function(e) {
+    let that = this;
     if (parseInt(this.data.quotedPrice) > 0) {
-      this.data.biddinglist.forEach((o, index) => {
-        if (o.bidTaskNum === this.data.bidTaskNum) {
-          this.data.biddinglist.splice(index, 1);
+      wx.request({
+        url: 'http://10.2.65.73:8080/STS/auctionTask/auctionPrice',
+        method: 'post',
+        data: {
+          userId: that.data.userId,
+          bidTaskId: that.data.bidTaskId,
+          quotedPrice: that.data.quotedPrice
+        },
+        header: {
+          'content-type': 'application/json'
+        },
+        success: function (res) {
+          if (res) {
+            that.data.biddinglist.forEach((o, index) => {
+              if (o.bidTaskId === that.data.bidTaskId) {
+                that.data.biddinglist.splice(index, 1);
+              }
+            })
+            that.setData({
+              biddinglist: that.data.biddinglist,
+              hiddenmodalput: true
+            })
+          }
         }
-      })
-      this.setData({
-        biddinglist: this.data.biddinglist,
-        hiddenmodalput: true
       })
     } else {
       this.setData({
         placeholder: '竞拍价格式不正确',
         quotedPrice: ''
       })
+    }
+  },
+  // ajax未竞价任务单数据
+  ajaxBidding() {
+    let that = this;
+    wx.request({
+      url: 'http://10.2.65.73:8080/STS/auctionTask/currentTask',
+      method: 'get',
+      data: {
+        userId: that.data.userId,
+        pageSize: that.data.biddingpageSize,
+        pickArea: that.data.pick,
+        deliverArea: that.data.deliver
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function(res) {
+        that.setData({
+          biddinglist: res.data.rows,
+          biddingtotal: res.data.total
+        })
+      }
+    })
+  },
+  // ajax竞价任务单数据
+  ajaxBidded() {
+    let that = this;
+    wx.request({
+      url: 'http://10.2.65.73:8080/STS/auctionTask/findBidded',
+      method: 'get',
+      data: {
+        userId: that.data.userId,
+        pageSize: that.data.biddedpageSize,
+        pickArea: that.data.pick,
+        deliverArea: that.data.deliver
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function(res) { 
+        that.setData({
+          biddedlist: res.data.rows,
+          biddedtotal: res.data.total
+        })
+      }
+    })
+  },
+  // 下拉刷新
+  onPullDownRefresh() {
+    if (parseInt(this.data.activeIndex) === 0) {
+      this.setData({
+        biddingpageSize: 2
+      })
+      this.ajaxBidding();
+    } else {
+      this.setData({
+        biddedpageSize: 2
+      })
+      this.ajaxBidded();
+    }
+    wx.stopPullDownRefresh();
+  },
+  // 上拉加载
+  onReachBottom() {
+    if (this.data.activeIndex === 0) {
+      if (this.data.biddingpageSize < this.data.biddingtotal) {
+        let page = this.data.biddingpageSize + 2;
+        this.setData({
+          biddingpageSize: page
+        })
+        this.ajaxBidding();
+      }
+    } else {
+      if (this.data.biddedpageSize < this.data.biddedtotal) {
+        let pages = this.data.biddedpageSize + 2;
+        this.setData({
+          biddedpageSize: pages
+        })
+        this.ajaxBidded();        
+      }
     }
   }
 })
--- utils/util.js
+++ utils/util.js
@@ -13,7 +13,106 @@ const formatNumber = n => {
   n = n.toString()
   return n[1] ? n : '0' + n
 }
-
+    // biddinglist: [{
+    //   bidTaskNum: "RW181221162007016",
+    //   releaseTime: "2018-12-21 16:20:13",
+    //   pickArea: "湖北省-天门市-天门市",
+    //   deliverArea: "湖北省-天门市-天门市",
+    //   taskType: "冷藏",
+    //   serviceTime: "78",
+    //   totalQuantity: "2",
+    //   totalWeight: "33",
+    //   totalVolume: "22",
+    //   proposedPrice: "133",
+    //   sealedDiskTime: "50"
+    // }, {
+    //   bidTaskNum: "RW181221162007015",
+    //   releaseTime: "2018-12-21 16:20:13",
+    //   pickArea: "湖北省-天门市-天门市",
+    //   deliverArea: "湖北省-天门市-天门市",
+    //   taskType: "冷藏",
+    //   serviceTime: "78",
+    //   totalQuantity: "2",
+    //   totalWeight: "33",
+    //   totalVolume: "22",
+    //   proposedPrice: "133",
+    //   sealedDiskTime: "4000"
+    // }, {
+    //   bidTaskNum: "RW181221162007015",
+    //   releaseTime: "2018-12-21 16:20:13",
+    //   pickArea: "湖北省-天门市-天门市",
+    //   deliverArea: "湖北省-天门市-天门市",
+    //   taskType: "冷藏",
+    //   serviceTime: "78",
+    //   totalQuantity: "2",
+    //   totalWeight: "33",
+    //   totalVolume: "22",
+    //   proposedPrice: "133",
+    //   sealedDiskTime: "4000"
+    // }, {
+    //   bidTaskNum: "RW181221162007015",
+    //   releaseTime: "2018-12-21 16:20:13",
+    //   pickArea: "湖北省-天门市-天门市",
+    //   deliverArea: "湖北省-天门市-天门市",
+    //   taskType: "冷藏",
+    //   serviceTime: "78",
+    //   totalQuantity: "2",
+    //   totalWeight: "33",
+    //   totalVolume: "22",
+    //   proposedPrice: "133",
+    //   sealedDiskTime: "4000"
+    // }, {
+    //   bidTaskNum: "RW181221162007015",
+    //   releaseTime: "2018-12-21 16:20:13",
+    //   pickArea: "湖北省-天门市-天门市",
+    //   deliverArea: "湖北省-天门市-天门市",
+    //   taskType: "冷藏",
+    //   serviceTime: "78",
+    //   totalQuantity: "2",
+    //   totalWeight: "33",
+    //   totalVolume: "22",
+    //   proposedPrice: "133",
+    //   sealedDiskTime: "4000"
+    // }, {
+    //   bidTaskNum: "RW181221162007015",
+    //   releaseTime: "2018-12-21 16:20:13",
+    //   pickArea: "湖北省-天门市-天门市",
+    //   deliverArea: "湖北省-天门市-天门市",
+    //   taskType: "冷藏",
+    //   serviceTime: "78",
+    //   totalQuantity: "2",
+    //   totalWeight: "33",
+    //   totalVolume: "22",
+    //   proposedPrice: "133",
+    //   sealedDiskTime: "4000"
+    // }],
+    // biddedlist: [{
+    //   bidTaskNum: "RW181221162007017",
+    //   releaseTime: "2018-12-21 16:20:13",
+    //   pickArea: "湖北省-天门市-天门市",
+    //   deliverArea: "湖北省-天门市-天门市",
+    //   taskType: "冷藏",
+    //   serviceTime: "78",
+    //   totalQuantity: "2",
+    //   totalWeight: "33",
+    //   totalVolume: "22",
+    //   proposedPrice: "133",
+    //   quotedPrice: "124",
+    //   sealedDiskTime: "50"
+    // }, {
+    //   bidTaskNum: "RW181221162007018",
+    //   releaseTime: "2018-12-21 16:20:13",
+    //   pickArea: "湖北省-天门市-天门市",
+    //   deliverArea: "湖北省-天门市-天门市",
+    //   taskType: "冷藏",
+    //   serviceTime: "78",
+    //   totalQuantity: "2",
+    //   totalWeight: "33",
+    //   totalVolume: "22",
+    //   proposedPrice: "133",
+    //   quotedPrice: "124",
+    //   sealedDiskTime: "110"
+    // }]
 module.exports = {
   formatTime: formatTime
 }
